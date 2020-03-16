49aacff2bf565c1d3f3f33c83e66b15a222eb3c7
Adrian <59113139+adrian-gvr@users.noreply.github.com>
2020-03-16 23:06:35 +0100

    Update and rename array/words.js to  array/words.js

--- array/words.js
+++ array/words.js
@@ -1,1 +1,9 @@
 
+let parole = ["Vai","dove","ti","porta","il","cuore","!"];
+let paroleStr = JSON.parse(JSON.stringify(parole));
+
+let paroleLtR = parole.join();
+let paroleRtL = paroleStr.reverse();
+
+console.log(paroleLtR);
+console.log(paroleRtL);
