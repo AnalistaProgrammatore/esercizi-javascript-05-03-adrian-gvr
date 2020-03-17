161ff18e60ca7e603e4727dfa6cf0f158f9e2eb2
Adrian <59113139+adrian-gvr@users.noreply.github.com>
2020-03-17 12:02:24 +0100

    Update and rename array/weekly.js to  array/weekly.js

--- array/weekly.js
+++ array/weekly.js
@@ -1,1 +1,25 @@
+function weekTemps(){
+  let this.dataStore = [];
+  this.add = add;
+  this.mediaMensile = mediaMensile;
+  this.mediaSettimanale = mediaSettimanale;
+}
+function add(settimana,temp){
+  this.dataStore[settimana - 1].push(temp);
+}
+function mediaMensile(){
+  let totale = 0;
+  let elementi =0;
+  for (let i = 0;i<this.dataStore.length; i++){
+    for(let j = 0; j<this.dataStore[i].length;j++){
+      totale += this.dataStore[i][j];
+    }
+  }
+  totale = 0;
+  elementi = 0;
+}
+function mediaSettimanale(settimana){
+  let totale = 0;
+  for (let j = 0; i<dataStore.[settimana - 1].length; j++){
+    
 
