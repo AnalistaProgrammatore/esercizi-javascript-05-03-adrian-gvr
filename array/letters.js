b90b96ee3d2a5b3d69e4db75120374eb86e61c46
Adrian <59113139+adrian-gvr@users.noreply.github.com>
2020-03-17 09:20:22 +0100

    Update and rename array/letters.js to  array/letters.js

--- array/letters.js
+++ array/letters.js
@@ -1,1 +1,11 @@
-
+function generaParola(){
+  let this.lettere = [];
+  this.unaParola() = unaParola();
+}
+function unaParola(...lettera){
+  this.lettere.push(...lettera);
+  return lettere.join("");
+}
+let parola = new generaParole;
+parola.unaParola("H","o","p","e","!");
+console.log("The word of the day is: ",parola);
